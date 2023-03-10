dd2963a027ffbd6051f9c7d0d6cdf0a5759aed97
Germain Barret <gbarret@mappingsuite.com>
2023-03-10 10:48:31 +0100

    Make it windows compatible, WIP. This script is no python, it's jython, needs more that a python env.

--- build.py
+++ build.py
@@ -1,6 +1,11 @@
-#!/bin/python
+#!/usr/bin/env python
 
 ## Jython script
-Debug.on(3) # run the script with option -c to get the log messages
+
+## Build jar version of this sikuli script (is multiplatform)
+
+#Debug.on(3) # run the script with option -c to get the log messages
 import org.sikuli.ide.SikulixIDE as IDE
-IDE.makeScriptjar(["plain", "/usr/local/src/AutoSell"])
+
+print("Building archive...")
+IDE.makeScriptjar(["plain", "/usr/local/src/auto_sell"])
